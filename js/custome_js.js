1bde144139c39b61dd54202ed758e616ba54133c
vikas ombale <39273213+vikasombale@users.noreply.github.com>
2020-04-02 02:01:14 +0530

    Add files via upload

--- js/custome_js.js
+++ js/custome_js.js
@@ -140,3 +140,22 @@ jQuery( document ).ready( function() {
 
 
 
+/* -------------- number -------------*/
+
+function increaseValue() {
+  var value = parseInt(document.getElementById('number').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value++;
+  document.getElementById('number').value = value;
+}
+
+function decreaseValue() {
+  var value = parseInt(document.getElementById('number').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value < 1 ? value = 1 : '';
+  value--;
+  document.getElementById('number').value = value;
+}
+
+
+/*----------------- number -----------------*/
